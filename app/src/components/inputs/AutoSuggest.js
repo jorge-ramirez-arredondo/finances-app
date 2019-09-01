e6219b8694a079229b09ecf3f6c04c0d7f359ec7
Jorge Ramirez <me.mark.two@gmail.com>
2019-09-01 16:23:03 -0400

    Add autosuggest component based on MUI implementation

---
+++ app/src/components/inputs/AutoSuggest.js
@@ -0,0 +1,171 @@
+import React from 'react';
+import deburr from 'lodash/deburr';
+import Autosuggest from 'react-autosuggest';
+import match from 'autosuggest-highlight/match';
+import parse from 'autosuggest-highlight/parse';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles } from '@material-ui/core/styles';
+
+function renderInputComponent(inputProps) {
+  const { classes, inputRef = () => {}, ref, ...other } = inputProps;
+
+  return (
+    <TextField
+      fullWidth
+      InputProps={{
+        inputRef: node => {
+          ref(node);
+          inputRef(node);
+        },
+        classes: {
+          input: classes.input,
+        },
+      }}
+      {...other}
+    />
+  );
+}
+
+const genRenderSuggestion = (getItemText) => (suggestion, { query, isHighlighted }) => {
+  const matches = match(getItemText(suggestion), query);
+  const parts = parse(getItemText(suggestion), matches);
+
+  return (
+    <MenuItem selected={isHighlighted} component="div">
+      <div>
+        {parts.map(part => (
+          <span key={part.text} style={{ fontWeight: part.highlight ? 500 : 400 }}>
+            {part.text}
+          </span>
+        ))}
+      </div>
+    </MenuItem>
+  );
+};
+
+function getDisplaySuggestions(suggestions, getItemText, value = "") {
+  const inputValue = deburr(value.trim()).toLowerCase();
+  const inputLength = inputValue.length;
+  let count = 0;
+
+  return inputLength === 0
+    ? []
+    : suggestions.filter(suggestion => {
+        const keep =
+          count < 5 && getItemText(suggestion).slice(0, inputLength).toLowerCase() === inputValue;
+
+        if (keep) {
+          count += 1;
+        }
+
+        return keep;
+      });
+}
+
+function defaultGetItemText(item) {
+  return item.label;
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: 250,
+    flexGrow: 1,
+  },
+  container: {
+    position: 'relative',
+  },
+  suggestionsContainerOpen: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing(1),
+    left: 0,
+    right: 0,
+  },
+  suggestion: {
+    display: 'block',
+  },
+  suggestionsList: {
+    margin: 0,
+    padding: 0,
+    listStyleType: 'none',
+  },
+  divider: {
+    height: theme.spacing(2),
+  },
+}));
+
+export default function AutoSuggest(props) {
+  const {
+    items,
+    inputValue = "",
+    getItemText = defaultGetItemText,
+    onChange,
+    label = "",
+    placeholder = "",
+    inputRef
+  } = props;
+
+  const classes = useStyles();
+  const [stateSuggestions, setSuggestions] = React.useState([]);
+
+  const handleSuggestionsFetchRequested = ({ value }) => {
+    setSuggestions(getDisplaySuggestions(items, getItemText, value));
+  };
+
+  const handleSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
+
+  const handleOnSuggestionSelected = (event, { suggestion, suggestionValue }) => {
+    typeof onChange === "function" && onChange(suggestionValue, suggestion);
+  };
+
+  const handleInputOnChange = (event, { newValue }) => {
+    typeof onChange === "function" && onChange(newValue, null);
+  };
+
+  const handleInputOnBlur = (event, { highlightedSuggestion }) => {
+    if (highlightedSuggestion && typeof onChange === "function") {
+      onChange(getItemText(highlightedSuggestion), highlightedSuggestion);
+    }
+  };
+
+  const autosuggestProps = {
+    renderInputComponent,
+    suggestions: stateSuggestions,
+    onSuggestionsFetchRequested: handleSuggestionsFetchRequested,
+    onSuggestionsClearRequested: handleSuggestionsClearRequested,
+    onSuggestionSelected: handleOnSuggestionSelected,
+    getSuggestionValue: getItemText,
+    renderSuggestion: genRenderSuggestion(getItemText)
+  };
+
+  return (
+    <Autosuggest
+      {...autosuggestProps}
+      inputProps={{
+        classes,
+        id: 'react-autosuggest-simple',
+        label,
+        placeholder,
+        value: inputValue,
+        onChange: handleInputOnChange,
+        onBlur: handleInputOnBlur
+      }}
+      theme={{
+        container: classes.container,
+        suggestionsContainerOpen: classes.suggestionsContainerOpen,
+        suggestionsList: classes.suggestionsList,
+        suggestion: classes.suggestion
+      }}
+      renderSuggestionsContainer={options => (
+        <Paper {...options.containerProps} square>
+          {options.children}
+        </Paper>
+      )}
+      ref={inputRef}
+    />
+  );
+}
