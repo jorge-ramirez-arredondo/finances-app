5128769b6d1f1a180f540a922ae14f2a60a2c878
Jorge Ramirez <me.mark.two@gmail.com>
2019-09-15 13:39:55 -0400

    Set up some default sorting for account and budget totals api calls

--- api/src/routes/dbs/routes/accountTotals.js
+++ api/src/routes/dbs/routes/accountTotals.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 // Get Account Totals
 router.get('/', async (req, res) => {
   const { db } = req;
-	const accounts = await db("AccountTotals");
+	const accounts = await db("AccountTotals").orderBy("name", "asc");
 
 	res.json(accounts);
 });
--- api/src/routes/dbs/routes/budgetTotals.js
+++ api/src/routes/dbs/routes/budgetTotals.js
@@ -3,7 +3,10 @@ const router = require("express").Router();
 // Get Budget Totals
 router.get('/', async (req, res) => {
   const { db } = req;
-  const budgets = await db("BudgetTotals");
+  const budgets = await db("BudgetTotals").orderBy([
+    { column: "accountID" },
+    { column: "name" }
+  ]);
 
   res.json(budgets);
 });
